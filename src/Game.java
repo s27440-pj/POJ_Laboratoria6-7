30f2d27a85bf912dd5cf8b084df4d8273020d806
krysiatokarska <s27440@pjwstk.edu.pl>
2023-06-17 14:03:50 +0200

    Adding a lopp in which generals are playin game

--- src/Game.java
+++ src/Game.java
@@ -1,21 +1,49 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
 
-        // insert scanner and ask for both general's names
-        General general1 = new General("Krysia");
+        // At the beginning I create 2 generals and soldiers for them for start
+        General general1 = new General("Christine");
+        General general2 = new General("Pablo");
 
-        // while loop with options to choose action by generals
-
-        //tests
         Soldier soldier1 = new Soldier(2, 4);
         Soldier soldier2 = new Soldier(1, 3);
         Soldier soldier3 = new Soldier(4, 5);
+
         general1.soldiers.add(soldier1);
         general1.soldiers.add(soldier2);
         general1.soldiers.add(soldier3);
-        general1.printSoldiers();
-        general1.maneuver();
-        general1.printSoldiers();
 
+        general2.soldiers.add(soldier1);
+        general2.soldiers.add(soldier2);
+        general2.soldiers.add(soldier3);
+
+        // while loop with options to choose action by generals
+        int action1 = 0;
+        int action2 = 0;
+        while (action1 != 6 || action2 !=6) {
+            menu();
+            System.out.println("General " + general1.getName() + ", these are possible actions you ca do. " +
+                    "Please eneter a number of action you want to perform: ");
+            Scanner scanner = new Scanner(System.in);
+            action1 = scanner.nextInt();
+            general1.actions(general2, action1);
+            System.out.println("End of your turn " + general1.getName());
+            menu();
+            System.out.println("General " + general2.getName() + ", these are possible actions you ca do. " +
+                    "Please eneter a number of action you want to perform: ");
+            action2 = scanner.nextInt();
+            general2.actions(general1, action2);
+            System.out.println("End of your turn " + general2.getName());
+        }
+    }
+    public static void menu(){
+        System.out.println("1 - maneuver");
+        System.out.println("2 - attack");
+        System.out.println("3 - buy soldier");
+        System.out.println("4 - check coins");
+        System.out.println("5 - end round");
+        System.out.println("6 - end game");
     }
 }
--- src/General.java
+++ src/General.java
@@ -27,7 +27,7 @@ public class General {
         int degree;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter degree of soldier you want to buy.\n 1 (private), 2 (corporal), 3 (capitan), 4(major)" +
-                "\nThe cost of one soldier is 10*degree");
+                "\nThe cost of one soldier is 10*degree: ");
         degree = scanner.nextInt();
         while (degree > 4 || degree < 1){
             System.out.println("Degree can be 1, 2, 3 or 4. You typed " + degree);
@@ -65,7 +65,7 @@ public class General {
     public void maneuver(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Do you want all army to take part in maneuver? \nEnetr yes/no: ");
-        String decision = scanner.next();
+        String decision = scanner.nextLine();
         int cost = 0;
         if (decision.equals("yes")){
             Iterator<Soldier> iterator = soldiers.iterator();
@@ -74,6 +74,7 @@ public class General {
                 soldier.experienceUp();
                 cost += soldier.getDegree();
             }
+            this.coins = this.coins - cost;
         }
         else if (decision.equals("no")) {
             List<Integer> soldiersInManeuver = new LinkedList<>();
@@ -94,10 +95,9 @@ public class General {
                 soldiers.get(number).experienceUp();
                 cost += soldiers.get(number).getDegree();
             }
-            scan.close();
+            this.coins = this.coins - cost;
         }
         else System.out.println("Invalid comand");
-        scanner.close();
     }
 
     public void generalLose(General general){
@@ -118,7 +118,7 @@ public class General {
             sum1 += soldier.getStrength();
         }
         for (Soldier soldier : general.soldiers){
-            sum1 += soldier.getStrength();
+            sum2 += soldier.getStrength();
         }
 
         if (sum1 > sum2){
@@ -135,5 +135,26 @@ public class General {
             this.soldiers.remove(random2);
         }
 
+    }
+    public void actions(General general, int action){
+        if (action == 1){
+            this.maneuver();
+            this.printSoldiers();
+        } else if (action == 2){
+            this.attack(general);
+            System.out.println("General's " + this.getName() + " soldiers");
+            this.printSoldiers();
+            System.out.println("General's " + general.getName() + " soldiers");
+            general.printSoldiers();
+        } else if (action == 3){
+            this.buySoldier();
+            this.printSoldiers();
+        } else if (action == 4){
+            System.out.println(this.getCoins());
+        } else if (action == 5){
+            System.out.println("You chose no action. Now is other's general turn.");
+        } else if (action == 6){
+            System.out.println("You chose to end the game.");
+        } else System.out.println("Wrong action number.");
     }
 }
